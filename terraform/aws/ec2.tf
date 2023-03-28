fab8e0d75defa5332b29961a6af8ac95aa53f3c8
Eugen Hinz <92438750+cloudnutzer@users.noreply.github.com>
2023-03-28 16:58:25 +0200

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -14,6 +14,8 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
 export AWS_SECRET_ACCESS_KEY=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
+  export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
+export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-west-2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
